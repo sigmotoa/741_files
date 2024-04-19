8731ef1138534343f86332898814acdac18c33d3
sigmotoa <si.galvismotoa@gmail.com>
2024-04-19 07:19:52 -0500

    Read a csv file

--- src/main/java/MainCSV.java
+++ src/main/java/MainCSV.java
@@ -1,2 +1,6 @@
 public class MainCSV {
+    public static void main(String[] args) {
+        ReadCSV paraLeer = new ReadCSV();
+        paraLeer.leerCSV("/Users/sigmotoa/Documents/UD/2024_1/objetos_741/codes/readingFiles/Reading/src/main/resources/notas.csv");
+    }
 }
--- src/main/java/ReadCSV.java
+++ src/main/java/ReadCSV.java
@@ -1,5 +1,36 @@
+import javax.swing.*;
 import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class ReadCSV {
     private BufferedReader lector;
+    private String linea;
+    private String registro[] = null;
+
+    public void leerCSV(String file)
+    {
+        try{
+            lector = new BufferedReader(new FileReader(file));
+            while ((linea=lector.readLine())!=null)
+            {
+                registro = linea.split(",");
+                imprimirRegistro();
+                System.out.println();
+            }
+            lector.close();
+            linea=null;
+            registro=null;
+        }
+        catch (Exception e)
+        {
+            JOptionPane.showMessageDialog(null,e);
+        }
+    }
+
+    public void imprimirRegistro()
+    {
+        for (int i = 0; i < registro.length; i++) {
+            System.out.print(registro[i]+"\t|\t");
+        }
+    }
 }
