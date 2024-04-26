83cfc6c6faae787949186836467789f699479cd9
sigmotoa <si.galvismotoa@gmail.com>
2024-04-26 07:11:49 -0500

    Write files

--- src/main/java/ReadWrite.java
+++ src/main/java/ReadWrite.java
@@ -1,2 +1,21 @@
+import java.io.BufferedReader;
+import java.io.FileWriter;
+
 public class ReadWrite {
+
+    public static void main(String[] args) {
+        String mensaje = "Inscribete a la Maraton";
+
+        try {
+            FileWriter archivo_out = new FileWriter("/Users/sigmotoa/Documents/UD/2024_1/objetos_741/codes/readingFiles/Reading/src/main/resources/output.txt");
+            archivo_out.write(mensaje);
+            archivo_out.close();
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error: "+e.getMessage());
+        }
+
+    }
+
 }
